82bf0892432382a51d7ef7b09d863a049c405e3e
Awaken <stewardsantos90@gmail.com>
2022-05-16 22:00:10 -0400

    using <select> for curso variable

--- nuevo.php
+++ nuevo.php
@@ -1,49 +1,70 @@
 <html lang="es">
-	<head>
+
+<head>
 	<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/bootstrap-theme.css" rel="stylesheet">
-		<script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-theme.css" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-3.1.1.js"
+		integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="./css/style.css">
-	</head>
-	
-	<body class="Body">
-	<form  method="POST"  autocomplete="off" class="form" >
+</head>
+
+<body class="Body">
+	<form method="POST" autocomplete="off" class="form">
 		<div class="container">
-		
-				<h3 >NUEVO REGISTRO</h3>
-					<label for="nombre" class="col-sm-2 control-label">Nombre</label>
-		
-						<input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre" required>
-					<label for="Curso" class="col-sm-2 control-label">Curso</label>
-				
-						<input type="text" class="form-control" id="curso" name="curso" placeholder="Curso" required>
-					<label for="telefono" class="col-sm-2 control-label">Telefono</label>
-				
-						<input type="text" class="form-control" id="telefono" name="telefono"  placeholder="Telefono">
-					<label for="combo" class="col-sm-2 control-label" required>Combo</label>
-					
-						<select class="form-control" id="combo" name="combo">
-							<option>Pizza con refresco</option>
-							<option>Nachos con queso y refresco</option>
-							<option>1 Croqueta, 1 Quipe, 1 Pastelito</option>
-                            <option>Palomitas con refresco</option>
-							<option>Donas con refresco</option>
-							<option>Sandwich con jugo natural</option>
-							<option>Sandwich de pizza con refresco</option>
-						</select>
-				<div class="form-group">
-				
-						<a href="index.php" class="btn btn-default">Regresar</a>
-						<button class="btn btn-primary" name='btn_enviar'>Guardar</button>
-					
-				</div>
-				</div>
-			</form>
+
+			<h3>NUEVO REGISTRO</h3>
+			<label for="nombre" class="col-sm-2 control-label">Nombre</label>
+
+			<input type="text" class="form-control" id="nombre" name="nombre" required>
+			<label for="Curso" class="col-sm-2 control-label">Curso</label>
+
+			<select class="form-control" id="curso" name="curso" required>
+				<optgroup label="Primaria">
+					<option>1ro de primaria</option>
+					<option>2do de primaria</option>
+					<option>3ro de primaria</option>
+					<option>4to de primaria</option>
+					<option>5to de primaria</option>
+					<option>6to de primaria</option>
+				</optgroup>
+				<optgroup label="Secundaria">
+					<option>1ro de secundaria</option>
+					<option>2do de secundaria</option>
+					<option>3ro de secundaria</option>
+					<option>4to de secundaria</option>
+					<option>5to de secundaria</option>
+					<option>6to de secundaria</option>
+				</optgroup>
+				<option>Ninguno</option>
+			</select>
+			<label for="telefono" class="col-sm-2 control-label">Telefono</label>
+
+			<input type="text" class="form-control" id="telefono" name="telefono" placeholder="Telefono">
+			<label for="combo" class="col-sm-2 control-label" required>Combo</label>
+
+			<select class="form-control" id="combo" name="combo" required>
+				<option>Pizza con refresco</option>
+				<option>Nachos con queso y refresco</option>
+				<option>1 Croqueta, 1 Quipe, 1 Pastelito</option>
+				<option>Palomitas con refresco</option>
+				<option>Donas con refresco</option>
+				<option>Sandwich con jugo natural</option>
+				<option>Sandwich de pizza con refresco</option>
+			</select>
+			<div class="form-group">
+
+				<a href="index.php" class="btn btn-default">Regresar</a>
+				<button class="btn btn-primary" name='btn_enviar'>Guardar</button>
+
+			</div>
+		</div>
+	</form>
 	<?php
 	include("guardar.php")
 	?>
-	</body>
+</body>
+
 </html>
