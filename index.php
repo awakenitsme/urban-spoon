90813a7ad829db205b3ccf8f0d201d080eb81307
Awaken <stewardsantos90@gmail.com>
2022-05-23 21:18:59 +0300

    Cleaning code

--- index.php
+++ index.php
@@ -102,14 +102,5 @@
 				</div>
 			</div>
 		</div>
-		
-		<script>
-			$('#confirm-delete').on('show.bs.modal', function(e) {
-				$(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
-				
-				$('.debug-url').html('Delete URL: <strong>' + $(this).find('.btn-ok').attr('href') + '</strong>');
-			});
-		</script>	
-		
 	</body>
 </html>
